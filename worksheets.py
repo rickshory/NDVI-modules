19738902f0e59eb8a43df97477219bd1d1353ce2
Rick Shory <rickshory@gmail.com>
2014-02-04 15:04:27 -0800

    basic structure and function of 'worksheets.py' cobbled together

--- worksheets.py
+++ worksheets.py
@@ -19,16 +19,21 @@ class dTreeCtrl(wx.TreeCtrl):
     
     def OnMouseEvent(self, event): 
         if event.LeftDown() and not self.HasFlag(wx.TR_MULTIPLE): 
-            ht_item, ht_flags = self.HitTest(event.GetPosition()) 
+            ht_item, ht_flags = self.HitTest(event.GetPosition())
+            print "LeftDown HitTest Item:", ht_item, "Flags:", ht_flags
             if (ht_flags & wx.TREE_HITTEST_ONITEM) != 0: 
                 self.SetFocus() 
                 self.SelectItem(ht_item) 
             else: 
                 event.Skip() 
         elif event.LeftUp(): 
-            ht_item, ht_flags = self.HitTest(event.GetPosition()) 
+            ht_item, ht_flags = self.HitTest(event.GetPosition())
+            print "LeftUp HitTest Item:", ht_item, "Flags:", ht_flags
             if (ht_flags & wx.TREE_HITTEST_ONITEM) != 0: 
-                print "OnMouseEvent -> LeftUp" 
+                print "OnMouseEvent -> LeftUp"
+#                ItemData = ht_item.GetItemData()
+                print "Dir ht_item:", dir(ht_item)
+                print "ht_item.m_pItem:", ht_item.m_pItem
         else: 
             event.Skip() 
 
@@ -46,19 +51,21 @@ class SetupWorksheetsPanel(wx.Panel):
         vSplit = wx.SplitterWindow(setupPanel, -1)
         treeViewPanel = wx.Panel(vSplit, -1)
 
-        # a combobox for tasks for the tree items
-        self.tTaskList = ['Add', 'Delete']
+#        # a combobox for tasks for the tree items
+#        self.tTaskList = ['Add', 'Delete']
 #        self.lblTaskList = wx.StaticText(self, label="Task")
-        self.trTaskCbx = wx.ComboBox(self, size=(95, -1), choices=self.tTaskList, style=wx.CB_DROPDOWN)
+#        self.trTaskCbx = wx.ComboBox(self, size=(95, -1), choices=self.tTaskList, style=wx.CB_DROPDOWN)
 #         self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.trTaskCbx)
 #         self.Bind(wx.EVT_TEXT, self.EvtText,self.trTaskCbx)
 #        wx.StaticText(treeViewPanel, -1, "This is where you'll see the tree view of datasets")
-        self.dsTree = dTreeCtrl(treeViewPanel, 1, wx.DefaultPosition, (-1,-1), wx.TR_HAS_BUTTONS|wx.TR_LINES_AT_ROOT)
+        self.dsTree = wx.TreeCtrl(treeViewPanel, 1, wx.DefaultPosition, (-1,-1), wx.TR_HAS_BUTTONS|wx.TR_LINES_AT_ROOT)
+#        self.dsTree = dTreeCtrl(treeViewPanel, 1, wx.DefaultPosition, (-1,-1), wx.TR_HAS_BUTTONS|wx.TR_LINES_AT_ROOT)
         dsRoot = self.dsTree.AddRoot('DataSets')
         ### 1. Register source's EVT_s to invoke launcher. ###
+#        wx.EVT_TREE_ITEM_ACTIVATED(self.dsTree, -1, self.dsTreeLeftClick)
         wx.EVT_TREE_ITEM_RIGHT_CLICK(self.dsTree, -1, self.dsTreeRightClick)
+        self.dsTree.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelChanged, id=1)
         self.tree_item_clicked = None
-
 
         trPnlSiz = wx.BoxSizer(wx.VERTICAL)
 #        trPnlSiz.Add(self.lblTaskList)
@@ -67,7 +74,7 @@ class SetupWorksheetsPanel(wx.Panel):
         treeViewPanel.SetSizer(trPnlSiz)
 
         detailsPanel = wx.Panel(vSplit, -1)
-        wx.StaticText(detailsPanel, -1, "This will have the details")
+        self.detailsLabel = wx.StaticText(detailsPanel, -1, "This will have the details")
         vSplit.SplitVertically(treeViewPanel, detailsPanel)
         hSiz = wx.BoxSizer(wx.HORIZONTAL)
         hSiz.Add(vSplit, 1, wx.EXPAND)
@@ -80,6 +87,18 @@ class SetupWorksheetsPanel(wx.Panel):
         vSiz = wx.BoxSizer(wx.VERTICAL)
         vSiz.Add(hSplit, 1, wx.EXPAND)
         self.SetSizer(vSiz)
+
+    def OnSelChanged(self, event):
+        print "OnSelChanged"
+        item = event.GetItem()
+        self.detailsLabel.SetLabel(self.dsTree.GetItemText(item))
+
+
+    def dsTreeLeftClick(self, event):
+        print "Item left clicked"
+        item = event.GetItem()
+        self.detailsLabel.SetLabel(self.dsTree.GetItemText(item))
+
 
     def dsTreeRightClick(self, event):
         self.tree_item_clicked = right_click_context = event.GetItem()
@@ -112,11 +131,12 @@ class SetupWorksheetsPanel(wx.Panel):
     def MenuSelectionCb( self, event ):
         # do something
         operation = menu_title_by_id[ event.GetId() ]
-        target    = self.list_item_clicked
+        print "operation:", operation
+#        target = self.tree_item_clicked
+        target = self.dsTree.GetItemText(self.tree_item_clicked)
+        print "target:", target
         print 'Perform "%(operation)s" on "%(target)s."' % vars()
         
-        
-
     def OnRightUp(self, event):
         index, flags = self.HitTest((x, y))
         print "index from HitTest", index
