39c92f2d4ba76df4fd0bd18d7456a1073ed2624d
Rick Shory <rickshory@gmail.com>
2014-06-13 16:01:44 -0700

    only count 'not unique' errors, ignore 'constraint failed'

--- add_data.py
+++ add_data.py
@@ -526,12 +526,13 @@ class DropTargetForFilesToParse(wx.FileDropTarget):
                             scidb.curD.execute(stSQL, (tsAsTime, lCh[iCol], lData[iCol]))
                             dataRecsAdded += 1 # count it
                         except sqlite3.IntegrityError: # item is already in Data table
+                            # distinguish duplicate error from invalid Value error
                             err_type, err_value, err_traceback = sys.exc_info()
-                            print repr(err_value)
-                            if repr(err_value) == "IntegrityError('constraint failed',)":
-                                print "Data Value is not numeric"
-                            else:
+                            if 'not unique' in repr(err_value):
                                 dataRecsDupSkipped += 1 # count but otherwise ignore
+                            else:
+                                # if Value is non-numeric, we get 'constraint failed'; junk, silently ignore
+                                print repr(err_value)
                         finally:
                             wx.Yield()
 
