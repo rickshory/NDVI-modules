e99bce8629287db7982d7f6f2243fffac6bc3457
Rick Shory <rickshory@gmail.com>
2014-10-07 08:19:33 -0700

    rearranged messages, comments

--- NDVI.py
+++ NDVI.py
@@ -1115,40 +1115,13 @@ class NDVIPanel(wx.Panel):
             
 
     def createDataSet(self):
-        print 'create dataset'
-        
         """
-
-        if UseOnlyValidNDVI:
-            check that min is less than max
-        Check that clear day has been entered
-        if not, give clue about floatover to preview
-        if not centered correctly, give clue about Longitude
-        if Excel output:
-            check for duplicate sheetnames in first 25 characters
-        get column header strings:
-            if UseRef:
-                get for IR ref
-                get for vis ref
-            get for IR data
-            get for vis data
-        get Stations list
-        get Dates list
-        get high and low cutoffs based on clear day, for IR and vis
-        write fn 'GetHighLowCutoffs' that gets numbers based on the percent high and low cutoffs for
-            a station/series/clearDay. Can use with either ref or data series.
-        write fn 'GetBeginEndTimes' based on a date, a longitude, and +/- hour cutoffs
-        write fn 'GetDaySpectralData' that fills table "_tmp_SpectralData" given
-            date, beginTime, endTime,
-            refStation, refIR, refVis,
-            datStation, datIR, datVis
-            
+        Creates NDVI datasets based on options selected in the form
         """
         if self.calcDict['ID'] == None:
-            stMsg = 'This is a new empty panel.\nIf you really want to use it, Save it first.'
-            dlg = wx.MessageDialog(self, stMsg, 'Empty')
-            result = dlg.ShowModal()
-            dlg.Destroy()
+            wx.MessageBox('This is a new empty panel.\nIf you really want ' \
+                'to use it, Save it first.', 'Empty',
+                wx.OK | wx.ICON_INFORMATION)
             return
         self.SavePanel() # attempt to save panel, will catch
                         #errors including missing/invalid CalcName
@@ -1223,6 +1196,31 @@ class NDVIPanel(wx.Panel):
                 self.tcNDVIvalidMax.SetFocus()
                 return
 
+        validation = """
+                Check that clear day has been entered
+        if not, give clue about floatover to preview
+        if not centered correctly, give clue about Longitude
+        if Excel output:
+            check for duplicate sheetnames in first 25 characters
+        get column header strings:
+            if UseRef:
+                get for IR ref
+                get for vis ref
+            get for IR data
+            get for vis data
+        get Stations list
+        get Dates list
+        get high and low cutoffs based on clear day, for IR and vis
+        write fn 'GetHighLowCutoffs' that gets numbers based on the percent high and low cutoffs for
+            a station/series/clearDay. Can use with either ref or data series.
+        write fn 'GetBeginEndTimes' based on a date, a longitude, and +/- hour cutoffs
+        write fn 'GetDaySpectralData' that fills table "_tmp_SpectralData" given
+            date, beginTime, endTime,
+            refStation, refIR, refVis,
+            datStation, datIR, datVis
+            
+
+        """
 
 
 class NDVIFrame(wx.Frame):
